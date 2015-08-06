2eaa6442f518f54c49203e6f577e102549c35708
NickRothbacher <nick.rothbacher@gmail.com>
2015-08-05 17:16:13 -0700

    Bug swatting

--- pumpControl.py
+++ pumpControl.py
@@ -138,7 +138,7 @@ def simultaneousMove(pump_waits, pump_steps, pump_m):
 				#	my_threads[y].start()
 				#	move_steps[y] -= 1
 			if(pump_steps[y] > 0 and pump_waits[y]%total_elapsed == 0):
-				pump_objs[y].move(pump_m[y])
+				pump_objs[y].move(pump_m[y], y)
 				pump_steps[y] -= 1
 
 
